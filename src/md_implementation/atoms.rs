3fc41b91424a06fefc6f50f9d1892351f36634f9
Jens Mueller <bla@gmx.de>
2024-08-09 12:53:55 +0200

    neighbors update & tests

--- src/md_implementation/atoms.rs
+++ src/md_implementation/atoms.rs
@@ -1,4 +1,6 @@
 use ndarray::{Array1, Array2};
+
+//use ndarray::{ArrayBase, DataMut, Dimension, Ix1, Ix2};
 #[allow(warnings)]
 #[derive(Clone)]
 
@@ -49,7 +51,106 @@ impl Atoms {
         }
     }
 
+    //pub fn resize(&mut self,new_num_elements:usize) {
+    //
+    //}
+
     fn print_type_of<T>(_: &T) {
         println!("type printing: {}", std::any::type_name::<T>())
     }
 }
+
+use ndarray::{ArrayBase, DataMut, Dim, Ix1, Ix2, OwnedRepr};
+
+pub trait ArrayExt<D> {
+    fn conservative_resize(&mut self, new_shape: D);
+}
+
+impl<S> ArrayExt<Ix1> for ArrayBase<S, Ix1>
+where
+    S: DataMut<Elem = i32>,
+    ArrayBase<S, Dim<[usize; 1]>>: From<ArrayBase<OwnedRepr<i32>, Dim<[usize; 1]>>>,
+{
+    fn conservative_resize(&mut self, new_shape: Ix1) {
+        let new_len = new_shape[0];
+        let old_len = self.len();
+
+        println!("old_len: {}", old_len);
+        println!("new_len: {}", new_len);
+        let mut new_array = Array1::zeros(new_len);
+
+        let min_len = old_len.min(new_len);
+
+        for i in 0..min_len {
+            new_array[i] = self[i] as i32;
+        }
+
+        // println!("Before resize:\n{:?}", self);
+        *self = new_array.to_owned().into();
+        // println!("After resize:\n{:?}", self);
+    }
+}
+
+impl<S> ArrayExt<Ix2> for ArrayBase<S, Ix2>
+where
+    S: DataMut<Elem = i32>,
+    ArrayBase<S, Dim<[usize; 2]>>: From<ArrayBase<OwnedRepr<i32>, Dim<[usize; 2]>>>,
+{
+    fn conservative_resize(&mut self, new_shape: Ix2) {
+        let (new_rows, new_cols) = (new_shape[0], new_shape[1]);
+        let (old_rows, old_cols) = self.dim();
+
+        let mut new_array = Array2::zeros((new_rows, new_cols));
+
+        let min_rows = old_rows.min(new_rows);
+        let min_cols = old_cols.min(new_cols);
+
+        for i in 0..min_rows {
+            for j in 0..min_cols {
+                new_array[(i, j)] = self[(i, j)] as i32;
+            }
+        }
+
+        println!("Before resize:\n{:?}", self);
+        *self = ArrayBase::from(new_array);
+        println!("After resize:\n{:?}", self);
+    }
+}
+
+/*
+
+trait ArrayExt
+{
+    fn conservativeResize(&mut self,  new_shape: D);
+}
+// impl ArrayExt for Array2<f64> {
+//     fn conservativeResize(&mut self, new_rows_cols: (usize, usize)){
+//         if self.ncols()==new_rows_cols.0 && self.nrows() == new_rows_cols.1 {
+//             return;
+//         }
+//         let mut vec = Array2::zeros(new_rows_cols);
+//         vec.assign(self);
+
+impl<S, D> ArrayExt for ArrayBase<S, D>
+where
+    S: DataMut<Elem = f64>,
+    D: Dimension,
+{
+    fn conservativeResize(&mut self, new_shape: D) {
+
+
+
+    }
+}
+*/
+// impl<T: Zero> ArrayExt for ndarray::ArrayBase<S,D>
+//     {
+//     fn conservativeResize(&mut self, new_rows_cols: (usize, usize)){
+//         if self.ncols()==new_rows_cols.0 && self.nrows() == new_rows_cols.1 {
+//             return;
+//         }
+//         let mut vec = Array2::zeros(new_rows_cols);
+//         vec.assign(self);
+//     }
+
+// }
--- src/md_implementation/neighbors.rs
+++ src/md_implementation/neighbors.rs
@@ -1,9 +1,10 @@
 use crate::md_implementation::atoms::Atoms;
-use ndarray::{s, Array1, Array2, Array3, ArrayView1, Axis, Ix2};
-use ndarray_stats::QuantileExt;
+use itertools::iproduct;
+use ndarray::{Array1, Array2, ArrayView1, Axis, Dim};
+use ndarray_linalg::norm::Norm;
+use std::cmp::Ordering;
 
 use super::atoms::ArrayExt;
-
 pub struct NeighborList {
     seed: Array1<i32>,
     neighbors: Array1<i32>,
@@ -18,16 +19,16 @@ impl NeighborList {
     pub fn update(&mut self, atoms: Atoms, cutoff: f64) -> (Array1<i32>, Array1<i32>) {
         println!("positions init: {:?}", atoms.positions);
         if atoms.positions.is_empty() {
-            //  self.seed.resize(0);
-            //  self.neighbors.resize(0);
-            //  return (self.seed,self.neighbors);
+            self.seed.conservative_resize(Dim(0));
+            self.neighbors.conservative_resize(Dim(0));
+            return (self.seed.clone(),self.neighbors.clone());
         }
 
         let mut origin = Array1::<f64>::from_elem(3, 3.0);
         let mut lengths = Array1::<f64>::from_elem(3, 3.0);
         let mut padding_lengths = Array1::<f64>::from_elem(3, 3.0);
 
-        let mut nb_grid_points = Array1::<i64>::from_elem(3, 3);
+        let mut nb_grid_points = Array1::<i32>::from_elem(3, 3);
 
         for (index_of_row, row) in atoms.positions.axis_iter(Axis(0)).enumerate() {
             let mut current_min = f64::INFINITY;
@@ -52,7 +53,7 @@ impl NeighborList {
         }
         println!("lengths {:?}", lengths);
         for (index_of_i, i) in (&lengths / cutoff).iter().enumerate() {
-            nb_grid_points[index_of_i] = i.ceil() as i64;
+            nb_grid_points[index_of_i] = i.ceil() as i32;
 
             //set to 1 if all atoms are in-plane
             println!("before: {}", nb_grid_points[index_of_i]);
@@ -82,39 +83,213 @@ impl NeighborList {
             col *= *grid_pts_per_len as f64 / length;
         }
 
-        let mut r = r.mapv(|i| i.floor() as i64);
+        let r = r.mapv(|i| i.floor() as i32);
         println!("r input to function atom_to_cell: {:?}", r);
 
-        let atom_to_cell: Array1<i64> = r
+        let atom_to_cell: Array1<i32> = r
             .axis_iter(Axis(1))
             .map(|col| Self::coordinate_to_index(col[0], col[1], col[2], nb_grid_points.view()))
             .collect();
         println!("atom_to_cell: {:?}", atom_to_cell);
 
-        let mut sorted_atom_indices = Array1::from_vec((0..atom_to_cell.len()).collect()).into_raw_vec();
-        println!("sorted_atom_indices with ingredients: {:?}",sorted_atom_indices);
+        let mut sorted_atom_indices =
+            Array1::from_vec((0..atom_to_cell.len()).collect()).into_raw_vec();
+        println!(
+            "sorted_atom_indices with ingredients: {:?}",
+            sorted_atom_indices
+        );
 
         //sort indices according to cell membership
         sorted_atom_indices.sort_by(|&i, &j| atom_to_cell[i].cmp(&atom_to_cell[j]));
-        println!("sorted_atom_indices actually sorted: {:?}",sorted_atom_indices);
+        println!(
+            "sorted_atom_indices actually sorted: {:?}",
+            sorted_atom_indices
+        );
 
-        let cell_index:i64 = atom_to_cell[sorted_atom_indices[0]];
-        let entry_index:i64 = 0;
-        let binned_atoms: Vec<(i64,i64)> = vec![(cell_index,entry_index)];
+        let mut cell_index: i32 = atom_to_cell[sorted_atom_indices[0]];
+        let mut entry_index: i32 = 0;
+        let mut binned_atoms: Vec<(i32, i32)> = vec![(cell_index, entry_index)];
 
-        println!("binned_atoms at initialization: {:?}",binned_atoms);
+        println!("binned_atoms at initialization: {:?}", binned_atoms);
+
+        for i in 1..sorted_atom_indices.len() {
+            if atom_to_cell[sorted_atom_indices[i]] != cell_index {
+                cell_index = atom_to_cell[sorted_atom_indices[i]];
+                entry_index = i as i32;
+                binned_atoms.push((cell_index, entry_index));
+            }
+        }
+
+        println!("binned_atoms: {:?}", binned_atoms);
+
+        println!("atoms.positions.len(): {}", atoms.positions.len());
+        self.seed
+            .conservative_resize(Dim(atoms.positions.shape()[1] + 1));
+
+        let mut neighborhood = Array2::<i32>::zeros((3, 27));
+
+        // Fill the array with combinations of x, y, z in the range -1 to 1
+        for (i, (x, y, z)) in iproduct!(-1..=1, -1..=1, -1..=1).enumerate() {
+            neighborhood[(0, i)] = x;
+            neighborhood[(1, i)] = y;
+            neighborhood[(2, i)] = z;
+        }
+
+        println!("Neighborhood: {:?}", neighborhood);
+
+        let mut n: usize = 0;
+        let cutoff_sq = cutoff * cutoff;
+                    let mut cou: i32 = 0;
+        for i in 0..atoms.positions.shape()[1] {
+            self.seed[i] = n as i32;
+
+            println!("=================\ncurrent seed: {:?}\ncurrent neighbors: {:?}\n======================",self.seed,self.neighbors);
 
 
 
+            println!(
+                "column: {:?};\norigin: {:?};\nlengths: {:?}",
+                &atoms.positions.column(i),
+                &origin.view(),
+                &lengths.view()
+            );
+            let cell_coord = (&nb_grid_points.mapv(|nb_grid_pt| nb_grid_pt as f64)
+                * (&atoms.positions.column(i) - &origin.view())
+                / &lengths.view())
+                .mapv(|coord_raw| coord_raw.floor() as i32);
+            println!("cell_coord: {:?}", cell_coord);
+            //let c = cell_coord.mapv(|c| c.floor() as i32);
+            //println!("cell_coord after flooring: {:?}",c);
 
+            for shift in neighborhood.axis_iter(Axis(1)) {
+                println!("new iteration in for neighboring cells .....");
+                let neigh_cell_coord: Array1<i32> = &cell_coord.view() + &shift;
 
-        return (Array1::from_elem(5, 0), Array1::from_elem(5, 0));
+                //skip if cell is out of bounds
+                if neigh_cell_coord.iter().position(|&x| x < 0) != None
+                    || neigh_cell_coord
+                        .iter()
+                        .zip(&nb_grid_points)
+                        .position(|(&neigh, &nb)| neigh >= nb)
+                        != None
+                {
+                    cou+=1;
+                    if cou == 12{
+                        println!("HERE...");
+                    }
+                    println!("CONTINUE --- Cell out of bounds; neigh cell coords: {:?}",neigh_cell_coord);
+                    continue;
+                }
+                let cell_index: i32 = Self::coordinate_to_index(
+                    neigh_cell_coord[0],
+                    neigh_cell_coord[1],
+                    neigh_cell_coord[2],
+                    nb_grid_points.view(),
+                );
+                println!("cell_index: {}", cell_index);
+
+                println!("binned_atoms: {:?}", binned_atoms);
+
+let cell_test = binned_atoms[binned_atoms
+                .binary_search_by((|&(x, _)| {
+                    if x < cell_index {
+                        return Ordering::Less;
+                    } else {
+                        return Ordering::Greater;
+                    }
+                })).unwrap_or_else(|x| x)];
+
+                println!("cell_test: {:?}",cell_test);
+
+                //Find first entry within the cell neighbor list
+                let res = 
+                // skip_if_no_lower_bound!(
+                    binned_atoms
+                    .binary_search_by(|&(x, _)| {
+                         if x == cell_index{
+                            return Ordering::Equal;
+                        }else if x < cell_index {
+                            return Ordering::Less;
+                        } else{
+                            return Ordering::Greater;
+                        }
+                    });
+
+                    let cell:(i32,i32) = match res{
+                        Ok(val) => binned_atoms[val],
+                        Err(_) => continue,
+                    };
+                // );
+
+                // let cell: (i32, i32) = (5,20);//binned_atoms[res];
+
+                println!("lower_bound cell: {:?}", cell);
+                println!("binned_atoms.end() quasi: {:?}",binned_atoms[0]);
+
+                // if cell == binned_atoms[0] || 
+                if cell.0 != cell_index {
+                    println!("CONTINUE --- cell == *binned_atoms.last().unwrap() || cell.0 != cell_index");
+                    continue;
+                }
+                let mut count = 0;
+                    println!("sorted_atom_indices: {:?}",sorted_atom_indices);
+                    let mut j = cell.1 as usize;
+                        println!("j before while : {}",j);
+                    while j < atom_to_cell.len()
+                        && atom_to_cell[sorted_atom_indices[j]] == cell_index
+                    {
+                        println!("j: {}",j);
+                        let neighi = sorted_atom_indices[j];
+                        println!("inside while loop: index j: {}; neighi=sorted_atom_indices: {}",j, neighi);
+                        if neighi == i {
+                            println!("CONTINUE --- neighi==i (inside while)");
+                            count +=1;
+                            // if count == 2{
+                                // println!("RETURN at neighi continue... ------------------------------------------------------------------------------------------");
+                            // return (self.seed.clone(),self.neighbors.clone());
+                            // }
+                            j+=1;
+                            continue;
+                        }
+                        let distance_vector = &atoms.positions.column(i).view()
+                            - &atoms.positions.column(neighi).view();
+                        println!("distance_vector: {:?}",distance_vector);
+                        let distance_sq = distance_vector.norm_l2().powi(2);
+                        println!("distance_sq: {}; cutoff_sq: {}",distance_sq,cutoff_sq);
+
+                        if distance_sq <= cutoff_sq {
+                            if n >= self.neighbors.len() {
+                                self.neighbors.conservative_resize(Dim(2 * self.neighbors.len()));
+                            }
+                            println!("neighbors[{}]={}",n,neighi);
+                            self.neighbors[n] = neighi as i32;
+                            n+=1;
+                        }
+
+                        j += 1;
+                    }
+                    println!("Outside while loop-----");
+            }
+        }
+        self.seed[atoms.positions.shape()[1]] = n as i32;
+        self.neighbors.conservative_resize(Dim(n));
+        return (self.seed.clone(),self.neighbors.clone());
+        // return (Array1::from_elem(5, 0), Array1::from_elem(5, 0));
+    }
+
+    pub fn nb_total_neighbors(&self)-> i32{
+        return self.seed[self.seed.len()-1];
+    }
+
+    pub fn nb_neighbors_of_atom(&self, i: usize)-> i32{
+        assert!(i<self.seed.len());
+        return self.seed[i+1]-self.seed[i];
     }
 
     fn coordinate_to_index_from_array(
-        c: Array1<i64>,
-        nb_grid_points: ArrayView1<i64>,
-    ) -> Array1<i64> {
+        c: Array1<i32>,
+        nb_grid_points: ArrayView1<i32>,
+    ) -> Array1<i32> {
         println!("nb_grid_pts(0): {}", nb_grid_points[0]);
         println!("nb_grid_pts: {}", nb_grid_points);
         Self::coordinate_to_index(c[0], c[1], c[2], nb_grid_points);
@@ -125,7 +300,7 @@ impl NeighborList {
         return Array1::from_elem(4, 2); // * (c.row(1) + nb_grid_points[1] * c.row(2));
     }
 
-    fn coordinate_to_index(x: i64, y: i64, z: i64, nb_grid_points: ArrayView1<i64>) -> i64 {
+    fn coordinate_to_index(x: i32, y: i32, z: i32, nb_grid_points: ArrayView1<i32>) -> i32 {
         return x + nb_grid_points[0] * (y + nb_grid_points[1] * z);
     }
 }
@@ -133,9 +308,9 @@ impl NeighborList {
 #[cfg(test)]
 mod tests {
     use crate::md_implementation::atoms::Atoms;
-    use googletest::{matchers::near, verify_that, assert_that};
+    use googletest::{assert_that, matchers::near, verify_that};
     use itertools::assert_equal;
-    use ndarray::Array2;
+    use ndarray::{Array1,Array2, s};
     use ndarray_rand::rand::SeedableRng;
     use ndarray_rand::{rand_distr::Uniform, RandomExt};
     use rand_isaac::isaac64::Isaac64Rng;
@@ -143,7 +318,7 @@ mod tests {
     use super::NeighborList;
 
     #[test]
-    fn test_neighbor_list() {
+    fn test_neighbor_list_4_atoms() {
         let mut atoms = Atoms::new(4);
         let new_positions = vec![0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 0.0, 0.0];
 
@@ -153,7 +328,57 @@ mod tests {
 
         let mut neighbor_list: NeighborList = NeighborList::new();
         let (seed, neighbors) = neighbor_list.update(atoms, 1.5);
-        assert_eq!(0,1);
+
+        assert_eq!(neighbor_list.nb_total_neighbors(),10);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(0),3);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(1),3);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(2),2);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(3),2);
+
+        // println!("neighbors: {:?}; seed[0]: {}, seed[1]: {}\nslice: {:?}",neighbors, seed[0],seed[1], neighbors.slice(s![seed[0]..seed[1]]));
+        assert_equal(neighbors.slice(s![seed[0]..seed[1]]).into_owned(), Array1::<i32>::from_vec(vec![3,1,2]));
+        assert_equal(neighbors.slice(s![seed[1]..seed[2]]).into_owned(), Array1::<i32>::from_vec(vec![3,0,2]));
+        assert_equal(neighbors.slice(s![seed[2]..seed[3]]).into_owned(), Array1::<i32>::from_vec(vec![0,1]));
+        assert_equal(neighbors.slice(s![seed[3]..seed[4]]).into_owned(), Array1::<i32>::from_vec(vec![0,1]));
+    }
+
+    #[test]
+    fn test_neighbor_list_3_atoms_first_atom_has_no_neighbor() {
+        let mut atoms = Atoms::new(4);
+        let new_positions = vec![0.0, 7.0, 0.0, 0.0, 0.0, 0.0, 7.0, 6.0, 0.0, 0.0, 0.0, 0.0];
+
+        let new_positions_arr = Array2::from_shape_vec((3, 4), new_positions)
+            .expect("Failed to create new positions array");
+        atoms.positions.assign(&new_positions_arr);
+
+        let mut neighbor_list: NeighborList = NeighborList::new();
+        let (seed, neighbors) = neighbor_list.update(atoms, 5.0);
+
+        println!("neighbors: {:?}",neighbors);
+
+        assert_eq!(neighbor_list.nb_total_neighbors(),2);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(0),0);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(1),0);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(2),1);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(3),1);
+    }
+
+#[test]
+    fn test_neighbor_list_3_atoms_last_atom_has_no_neighbor() {
+        let mut atoms = Atoms::new(3);
+        let new_positions = vec![0.0, 0.0, 0.0, 7.0, 6.0, 0.0, 0.0, 0.0, 0.0];
+
+        let new_positions_arr = Array2::from_shape_vec((3, 3), new_positions)
+            .expect("Failed to create new positions array");
+        atoms.positions.assign(&new_positions_arr);
+
+        let mut neighbor_list: NeighborList = NeighborList::new();
+        let (seed, neighbors) = neighbor_list.update(atoms, 5.0);
+
+        assert_eq!(neighbor_list.nb_total_neighbors(),2);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(0),1);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(1),1);
+        assert_eq!(neighbor_list.nb_neighbors_of_atom(2),0);
     }
 }
 
